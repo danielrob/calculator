ae8ae655d2167116cc42688f4ea863c104811cb3
Daniel Robinson <djr.self@gmail.com>
2015-11-11 10:54:16 -0500

    Bugfixes & Improvements

--- site/script.js
+++ site/script.js
@@ -4,7 +4,7 @@
   /*
     Screen
   */
-  function Screen(node) {
+  window.Screen = function(node) {
     this.node = node;
   }
 
@@ -14,6 +14,17 @@
     },
     clear: function() {
       this.node.innerHTML = null;
+    },
+    correction: function(char, count) {
+      count = count || 2;
+      var current = this.node.innerHTML;
+      this.write(current.substr(0, current.length - count) + char);
+    },
+    addChar: function(char) {
+      this.node.innerHTML += char;
+    },
+    read: function(){
+      return this.node.innerHTML;
     }
   };
 
@@ -22,13 +33,19 @@
   /*
     Calculator operates on a screen in response to its .newKey method calls.
   */
-  var Calculator = (function(screen) {
+  var Calculator = window.Calculator = (function(screen) {
     /*
       Numbr - so we can build decimal numbers from a stream of key inputs.
     */
     function Numbr(value) {
       this.value = value;
       this.decimals = 0;
+      this.startDecimaling = false;
+      this.negated = false;
+    }
+
+    function strip(number) {
+      return parseFloat(number.toPrecision(15));
     }
 
     Numbr.prototype = {
@@ -41,7 +58,10 @@
         if (this.startDecimaling) this.decimals++;
       },
       toDecimal: function() {
-        return this.value /  Math.pow(10, this.decimals);
+        this.value = this.value || 0;
+        var unNegated = this.value /  Math.pow(10, this.decimals);
+        var negated = this.negated ? -unNegated : unNegated;
+        return negated > Math.pow(10,14) ? negated.toExponential(14) : strip(negated);
       },
       startDecimal: function() {
         this.startDecimaling = true;
@@ -49,17 +69,26 @@
       },
       isValued: function() {
         return (this.value || this.value === 0);
+      },
+      negate: function() {
+        this.negated = !this.negated;
       }
     };
 
     // Init Calculator
     var firstNumbr = new Numbr(),
       secondNumbr = new Numbr(),
-      opPending = false;
+      opPending = false,
+      equalsApplied = false;
 
     function applyOperation() {
-      firstNumbr = opPending(firstNumbr, secondNumbr);
-      secondNumbr = new Numbr();
+      if (firstNumbr.isValued() && secondNumbr.isValued() && !!opPending) {
+        firstNumbr = opPending(firstNumbr, secondNumbr);
+        secondNumbr = new Numbr();
+        opPending = false;
+        return true;
+      }
+      return false;
     }
 
     function applyToCurrentNumber(fn) {
@@ -77,9 +106,12 @@
       "x": function multiply(a, b) {
         return new Numbr(a.toDecimal() * b.toDecimal());
       },
-      "%": function divide(a, b) {
+      "/": function divide(a, b) {
         return new Numbr(a.toDecimal() / b.toDecimal());
       },
+      "x^y": function powerOf(a, b) {
+        return new Numbr(Math.pow(a.toDecimal(), b.toDecimal()));
+      }
     };
 
     var functions = {
@@ -87,61 +119,152 @@
         firstNumbr = new Numbr();
         secondNumbr = new Numbr();
         opPending = false;
+        equalsApplied = false;
         screen.clear();
       },
       "CE": function clearEntry() {
-        applyToCurrentNumber(function(num) {
-          num = new Numbr();
+        if (!opPending) {
+          firstNumbr = new Numbr();
           screen.clear();
-        });
+        } else {
+          opPending = null;
+          secondNumbr = new Numbr();
+          screen.write(firstNumbr.toDecimal());
+        }
       },
       "=": function equals() {
-        if (!!opPending && secondNumbr.isValued()) {
-          applyOperation();
-          screen.write(firstNumbr.value);
-        }
+        applyOperation();
+        equalsApplied = true;
+        screen.clear();
+        setTimeout(function() {
+          screen.write(firstNumbr.toDecimal());
+        }, 15);
       },
       ".": function decimal() {
         applyToCurrentNumber(function(num) {
+          if (num.startDecimaling) screen.correction('', 1);
           num.startDecimal();
-          screen.write(num.toDecimal());
         });
+        equalsApplied = false;
       },
+      "%": function percent() {
+        var first;
+        applyToCurrentNumber(function(num) {
+          num.decimals += 2;
+        });
+        if (opPending === operations['+']) {
+          first = firstNumbr;
+          opPending = operations.x;
+          functions['=']();
+          opPending = operations['+'];
+          secondNumbr = first;
+          functions['=']();
+        } else if (opPending === operations['-']) {
+          first = firstNumbr;
+          opPending = operations.x;
+          functions['=']();
+          opPending = operations['-'];
+          secondNumbr = firstNumbr;
+          firstNumbr = first;
+          functions['=']();
+        } else {
+          functions['=']();
+        }
+      },
+      "+/-": function swapSign() {
+        if (!firstNumbr.isValued()) {
+          screen.clear();
+          return;
+        }
+        this['=']();
+        applyToCurrentNumber(function(num) {
+          num.negate();
+        });
+      }
     };
+
 
     return {
       sendKey: function(theKey) {
 
+
+        if (theKey !== '=') screen.addChar(theKey);
+
         if (typeof theKey === 'number') {
+
+          if (equalsApplied) {
+            firstNumbr = new Numbr();
+            opPending = false;
+            equalsApplied = false;
+            screen.write(theKey);
+          }
+
+
           applyToCurrentNumber(function(num) {
+            // Leading zeros problem
+            if (num.isValued() && !num.startDecimaling && num.value === 0) {
+              if (theKey === 0) {
+                screen.correction('', 1);
+              } else {
+                screen.correction(theKey);
+              }
+            }
+            // Build the number up
             num.shift(theKey);
-            screen.write(num.toDecimal());
           });
 
         } else if (typeof operations[theKey] === 'function') {
-          // A first number is required for an operation to be applicable.
-          if (!firstNumbr.isValued()) return;
-           // If we already have a (firstNumber, operation, secondNumber) triplet.
-          if (!!opPending && secondNumbr.isValued()) {
-            applyOperation();
-            screen.write(firstNumbr.toDecimal());
+
+          // Minus is special
+          if (theKey === "-") {
+            if (!firstNumbr.isValued()) {
+              firstNumbr.negate();
+              screen.correction('-');
+              return;
+            } else if (!!opPending && !secondNumbr.isValued() && !secondNumbr.negated) {
+              secondNumbr.negate();
+              return;
+            }
           }
-          // Set or Overwrite the next operation.
+
+          // No first number? Correct screen and get out of here.
+          if (!firstNumbr.isValued()) {
+            screen.correction('', 1);
+            return;
+          }
+
+          // Now, if triplet exists, reduce it & wipe pending operations.
+          applyOperation();
+
+          // Don't enter pending operation set/overwrite after negated number.
+          if (!!opPending && secondNumbr.negated) {
+            screen.correction('', 1);
+            return;
+          }
+
+          // Set / Overwrite the pending operation.
+          if (!!opPending) screen.correction(theKey);
           opPending = operations[theKey];
 
-        } else if (typeof functions[theKey] === 'function') {
-          functions[theKey]();
-        }
+          // Ensure we will accept new numbers after equals
+          equalsApplied = false;
 
+        } else if (typeof functions[theKey] === 'function') functions[theKey]();
+      },
+
+      error: function() {
+        functions.AC();
       }
     };
 
   })(screen);
 
 
-  (function createKeyEventHandlers() {
+
+
+  window.onload = function createKeyEventHandlers() {
     var keys =
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "AC", "CE", "=", ".", "%", "+", "-", "x"];
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "AC", "CE", "=", ".", "/", "+", "-", "x", "x^y", "+/-", "%"];
     var createHandler = function(el, key) {
       el.addEventListener("click", function() {
         Calculator.sendKey(key);
@@ -151,6 +274,6 @@
       var key = keys[i];
       createHandler(document.getElementById(key), key);
     }
-  })();
+  };
 
 })();
